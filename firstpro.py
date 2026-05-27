78bd44fbbb9ae91a2dc27ede33dc055bd995c1e6
shreya254 <shreyasshsn.1@gmail.com>
2026-05-27 18:03:26 +0530

    first day

---
+++ firstpro.py
@@ -0,0 +1,17 @@
+print("Hello world")
+a=2
+b=5
+sum = (a+b)
+print(sum)
+num1 =int(input("enter a num"))
+num2 = int(input("enter a num"))
+print(num1+num2)
+
+side = float(input("enter the value"))
+print(side*side)
+
+x = float(input("enter the value"))
+y= float(input("enter thr value"))
+avg = (x+y)/2
+print(avg)
+print(a>=b)
