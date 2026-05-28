d8430a8eb864173e382a2a1e8ce1fe0d2529cc26
shreya254 <shreyasshsn.1@gmail.com>
2026-05-28 21:58:09 +0530

    if else programme

---
+++ thirdpro.py
@@ -0,0 +1,27 @@
+a= int(input("Enter a number"))
+if(a%2==0):
+    print("even")
+else :
+    print("odd")
+
+
+x = 15
+y=8
+z= 10
+if(x>y):
+    if(x>z):
+        print(x)
+    else:
+        print(z)
+
+elif(y>z):
+    print(y)
+else:
+    print(z)
+
+
+if(a%7==0):
+    print(a, "is a multiple of 7")
+else:
+    print(a , "is not a multiple of 7")
+
